3f0ca095637c843a705bbc00a135299d5820be81
Harsh Upadhye <harshupadhye2832@gmail.com>
2023-05-05 19:09:40 +0530

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import SignUp from './Components/Register/SignUp'
 
 function App() {
 
   return (
     <>
-      <div>
-      <h1>Hello world </h1>
-       </div>
-    
+    <SignUp/>
     </>
   )
 }
