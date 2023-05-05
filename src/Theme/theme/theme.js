b3b831c75bff3c5872529a462d9b4363562c0dd6
Harsh Upadhye <harshupadhye2832@gmail.com>
2023-05-05 19:09:30 +0530

    styles added

---
+++ src/Theme/theme/theme.js
@@ -0,0 +1,6 @@
+import { extendTheme } from "@chakra-ui/react";
+import { globalStyles } from "../Styles/styles";
+// import { mode } from "@chakra-ui/theme-tools";
+export default extendTheme(
+  globalStyles,
+);
