2933f6d7154c4b4d659faee2892dc3d8db75a12c
sid120 <siddharthrakshe12@gmail.com>
2023-05-07 03:15:15 +0530

    created category

--- src/views/dashboard/Trophy.js
+++ src/views/dashboard/Trophy.js
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import CardContent from '@mui/material/CardContent'
 import { styled, useTheme } from '@mui/material/styles'
+import { Stack } from '@mui/material'
 
 // Styled component for the triangle shaped background image
 const TriangleImg = styled('img')({
@@ -14,35 +15,59 @@ const TriangleImg = styled('img')({
 })
 
 // Styled component for the trophy image
-const TrophyImg = styled('img')({
-  right: 36,
-  bottom: 20,
-  height: 98,
-  position: 'absolute'
-})
-
 const Trophy = () => {
   // ** Hook
   const theme = useTheme()
   const imageSrc = theme.palette.mode === 'light' ? 'triangle-light.png' : 'triangle-dark.png'
 
+  const SidebarContainer = styled('div')({
+    width: '300px', /* adjust the width as needed */
+    position: 'fixed',
+    top: 80,
+    right: 80,
+    bottom: 50,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  })
+
   return (
-    <Card sx={{ position: 'relative' }}>
-      <CardContent>
-        <Typography variant='h6'>Congratulations John! 🥳</Typography>
-        <Typography variant='body2' sx={{ letterSpacing: '0.25px' }}>
-          Best seller of the month
-        </Typography>
-        <Typography variant='h5' sx={{ my: 4, color: 'primary.main' }}>
-          $42.8k
-        </Typography>
-        <Button size='small' variant='contained'>
-          View Sales
-        </Button>
-        <TriangleImg alt='triangle background' src={`/images/misc/${imageSrc}`} />
-        <TrophyImg alt='trophy' src='/images/misc/trophy.png' />
-      </CardContent>
-    </Card>
+    <SidebarContainer>
+      <Card sx={{ position: 'relative' }}>
+        <CardContent>
+          <Stack direction="column" spacing={4}>
+            <Typography variant='h6' textAlign='center'>Categories</Typography>
+            <Button size='small' variant='contained'>
+              Technology
+            </Button>
+            <Button size='small' variant='contained'>
+              Food
+            </Button>
+            <Button size='small' variant='contained'>
+              Science
+            </Button>
+            <Button size='small' variant='contained'>
+              Web
+            </Button>
+          </Stack>
+          <TriangleImg alt='triangle background' src={`/images/misc/${imageSrc}`} />
+        </CardContent>
+      </Card>
+      <Card sx={{ position: 'relative' }}>
+        <CardContent>
+          <Stack direction="column" spacing={4}>
+            <Typography variant='h6' textAlign='center'>People you follow</Typography>
+            <Typography size='small' variant='contained'>
+              Makarand 
+            </Typography>
+            
+          </Stack>
+          <TriangleImg alt='triangle background' src={`/images/misc/${imageSrc}`} />
+        </CardContent>
+      </Card>
+    </SidebarContainer>
+    
+    
   )
 }
 
