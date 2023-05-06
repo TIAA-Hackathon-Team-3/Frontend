b81ad4c937cad7974d9af1c53fd9f0a8f3d8265a
Harsh Upadhye <harshupadhye2832@gmail.com>
2023-05-07 00:58:58 +0530

    API integrated

--- src/pages/pages/otp/index.js
+++ src/pages/pages/otp/index.js
@@ -1,5 +1,5 @@
 // ** React Imports
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 // ** Next Imports
 import Link from 'next/link'
@@ -58,52 +58,38 @@ const FormControlLabel = styled(MuiFormControlLabel)(({ theme }) => ({
   }
 }))
 
-const LoginPage = () => {
+const OtpPage = () => {
   // ** State
-  const [values, setValues] = useState({
-    showPassword: false
-  })
 
-  const [data, setdata] = useState({
-    email: "",
-    password: "",
-  });
+
+  const [otp, setOTP] = useState(['', '', '', '', '', '']); 
+  const refs = useRef([]);
+  const handleOTPChange = (e, index) => {
+    const value = e.target.value;
+    setOTP(prev => {
+      const newOTP = [...prev];
+      newOTP[index] = value;
+      return newOTP;
+    });
+    if (value.length === 1 && index < refs.current.length - 1) {
+        refs.current[index + 1].focus();
+      }
+  };
 
   // ** Hook
   const theme = useTheme()
   const router = useRouter()
 
-  const {email, password } = data;
 
-  const dataChange = (event) => {
-    const newData = { ...data, [event.target.name]: event.target.value };
-    setdata(newData);
-  }
-
-  const handleClickShowPassword = () => {
-    setValues({ ...values, showPassword: !values.showPassword })
-  }
   
   const handleSubmit = async(e) => {
     e.preventDefault();
-    try {
-      const result = await axios.post(
-        'https://backend-coral-nine.vercel.app/api/v1/login',
-        data
-      )
-      console.log(result);
-      if(result.data.success)
-      {
-        toast.success(result.data.message,{duration:5000});
-      }
-    } catch (error) {
-      toast.error(error.response?.data?.message ?? "An error occurred",{duration:5000})
-    }
+    const enteredOTP = otp.join(''); 
+    console.log('Entered OTP:', enteredOTP);
+
   }
 
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault()
-  }
+
 
   return (
     <Box className='content-center'>
@@ -183,51 +169,34 @@ const LoginPage = () => {
             </Typography>
           </Box>
           <Box sx={{ mb: 6 }}>
-            <Typography variant='h5' sx={{ fontWeight: 600, marginBottom: 1.5 }}>
-              Welcome to {themeConfig.templateName}! 🎊
+            <Typography variant='h5' sx={{ fontWeight: 600, marginBottom: 1.5, alignSelf :'center' }}>
+              OTP
             </Typography>
-            <Typography variant='body2'>Please sign-in to your account and start the adventure</Typography>
+            <Typography variant='body2'>Please enter OTP sent on Email.</Typography>
           </Box>
           <form noValidate autoComplete='off' onSubmit={e => e.preventDefault()}>
-            <TextField autoFocus fullWidth 
-            id='email' 
-            label='Email' 
-            sx={{ marginBottom: 4 }}
-            name='email'
-            value={data.email}
-            onChange={dataChange}
-            required
-            />
-            <FormControl fullWidth>
-              <InputLabel htmlFor='auth-login-password'>Password</InputLabel>
-              <OutlinedInput
-                label='Password'
-                value={data.password}
-                id='password'
-                name='password'
-                onChange={dataChange}
-                type={values.showPassword ? 'text' : 'password'}
-                endAdornment={
-                  <InputAdornment position='end'>
-                    <IconButton
-                      edge='end'
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      aria-label='toggle password visibility'
-                    >
-                      {values.showPassword ? <EyeOutline /> : <EyeOffOutline />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-              />
-            </FormControl>
-            <Box
-              sx={{ mb: 4, display: 'flex', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'space-between' }}
-            >
-              <FormControlLabel control={<Checkbox />} label='Remember Me' />
-              <Link passHref href='/'>
-                <LinkStyled onClick={e => e.preventDefault()}>Forgot Password?</LinkStyled>
-              </Link>
+            <Box sx={{ display: 'flex', justifyContent: 'center', marginBottom: 4 , alignItems:'center' }}>
+                {otp.map((digit, index) => (
+                    <TextField
+                        key={index}
+                        // type='number'
+                        inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', maxLength: 1 }}
+                        value={digit}
+                        onChange={(e) => handleOTPChange(e, index)}
+                        sx={{ marginX: 1, width: 50 }}
+                        ref={(el) => (refs.current[index] = el)}
+                        required
+                        onKeyUp={(e) => {
+                            if (e.key !== 'Backspace' && e.key !== 'Delete' && e.key !== 'Shift') {
+                              if (refs.current[index + 1]) {
+                                refs.current[index + 1].focus();
+                              }
+                            } else if (e.key === 'Backspace' && refs.current[index - 1]) {
+                              refs.current[index - 1].focus();
+                            }
+                          }}
+                    />
+                ))}
             </Box>
             <Button
               fullWidth
@@ -236,30 +205,12 @@ const LoginPage = () => {
               sx={{ marginBottom: 7 }}
               onClick={handleSubmit}
             >
-              Login
+              Verify
             </Button>
             <Box sx={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'center' }}>
-              <Typography variant='body2' sx={{ marginRight: 2 }}>
-                New on our platform?
+              <Typography variant='body2' sx={{ marginRight: 2, cursor: 'pointer' }}>
+                Resent OTP
               </Typography>
-              <Typography variant='body2'>
-                <Link passHref href='/pages/register'>
-                  <LinkStyled>Create an account</LinkStyled>
-                </Link>
-              </Typography>
-            </Box>
-            <Divider sx={{ my: 5 }}>or</Divider>
-            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-              <Link href='/' passHref>
-                <IconButton component='a' onClick={e => e.preventDefault()}>
-                  <Facebook sx={{ color: '#497ce2' }} />
-                </IconButton>
-              </Link>
-              <Link href='/' passHref>
-                <IconButton component='a' onClick={e => e.preventDefault()}>
-                  <Google sx={{ color: '#db4437' }} />
-                </IconButton>
-              </Link>
             </Box>
           </form>
         </CardContent>
@@ -272,6 +223,6 @@ const LoginPage = () => {
     </Box>
   )
 }
-LoginPage.getLayout = page => <BlankLayout>{page}</BlankLayout>
+OtpPage.getLayout = page => <BlankLayout>{page}</BlankLayout>
 
-export default LoginPage
+export default OtpPage
