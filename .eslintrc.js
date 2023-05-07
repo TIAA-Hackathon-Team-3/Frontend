ce2ff90e93d82666617b3c1c77d8a032973d8afa
Akash-Tandale001 <ak.tandale123@gmail.com>
2023-05-07 19:13:18 +0530

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,37 +17,7 @@ module.exports = {
     }
   },
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/display-name': 'off',
-    '@next/next/no-img-element': 'off',
-    'react/no-unescaped-entities': 'off',
-    'import/no-anonymous-default-export': 'off',
-
-    // add new line above comment
-    'lines-around-comment': [
-      'error',
-      {
-        beforeLineComment: true,
-        beforeBlockComment: true,
-        allowBlockStart: true,
-        allowClassStart: true,
-        allowObjectStart: true,
-        allowArrayStart: true
-      }
-    ],
-
-    // add new line above return
-    'newline-before-return': 'error',
-
-    // add new line below import
-    'import/newline-after-import': [
-      'error',
-      {
-        count: 1
-      }
-    ],
-
+    
     
   }
 }
