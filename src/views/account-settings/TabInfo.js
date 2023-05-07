fc0df6c48761e77d474e7a8b289344d02c37db9f
Akash-Tandale001 <ak.tandale123@gmail.com>
2023-05-07 18:54:06 +0530

    interagrated cloudninary

--- src/views/account-settings/TabInfo.js
+++ src/views/account-settings/TabInfo.js
@@ -1,5 +1,5 @@
 // ** React Imports
-import { forwardRef, useState } from 'react'
+import { forwardRef, useEffect, useState } from 'react'
 
 // ** MUI Imports
 import Grid from '@mui/material/Grid'
@@ -22,6 +22,7 @@ import DatePicker from 'react-datepicker'
 
 // ** Styled Components
 import DatePickerWrapper from 'src/@core/styles/libs/react-datepicker'
+// import cloudinary from 'src/utils/cloudinary'
 
 const CustomInput = forwardRef((props, ref) => {
   return <TextField inputRef={ref} label='Birth Date' fullWidth {...props} />
@@ -34,95 +35,94 @@ const TabInfo = () => {
     image: "",
     category: ""
   })
-  const [image, setImage] = useState("");
-  const handleImageChange = async () => {
-    const data = new FormData();
-    data.append("file", data.image);
-    data.append("upload_preset", "ml_default");
-    data.append("cloud_name", "dd9cmhunr");
-
-    const res = await axios.post(`https://api.cloudinary.com/v1_1/dd9cmhunr/image/upload`, data);
-    
-    console.log(res);
-  }
+ 
   
+  const handleImageChange = async (event) => {
+
+    const data1 = new FormData();
+    data1.append("file", event.target.files[0]);
+    data1.append("upload_preset", "wqhyebxb");
+
+    const res = await axios.post(`https://api.cloudinary.com/v1_1/dr06eyczk/image/upload`, data1);
+    setData({...data , image: res.data.url});
+  }
+
   const handaleChange = (event) => {
     setData({
       ...data,
       [event.target.name]: event.target.value
     })
   }
-    const handleOnClick = async () => {
-      await handleImageChange();
-      debugger
-      const createPost = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/createPost/${userId}`,
-        data, {
-        headers: {
-          'x-access-token': `${token}`
-        }
-      })
-      console.log(createPost);
-    }
+  const handleOnClick = async () => {
+    await handleImageChange();
+    const createPost = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/createPost/${userId}`,
+      data, {
+      headers: {
+        'x-access-token': `${token}`
+      }
+    })
+    console.log(createPost);
+  }
 
-    return (
-      <CardContent>
-        <form>
-          <Grid container spacing={7}>
+  return (
+    <CardContent>
+      <form>
+        <Grid container spacing={7}>
 
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label='Title'
-                name="title"
-                placeholder='Your title Is here'
-                onChange={handaleChange}
-              />
-            </Grid>
-
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                multiline
-                name="discription"
-                label='Description'
-                minRows={9}
-                onChange={handaleChange}
-                placeholder='Your content Is here'
-              />
-            </Grid>
-
-            <Grid item xs={12} sm={6}>
-              <FormControl fullWidth>
-                <InputLabel>Category</InputLabel>
-                <Select label='Category' name='category' >
-                  <MenuItem value='Sports'>Sports</MenuItem>
-                  <MenuItem value='Technology'>Technology</MenuItem>
-                  <MenuItem value='Education'>Education</MenuItem>
-                  <MenuItem value='Health'>Health</MenuItem>
-                  <MenuItem value='Finance'>Finance</MenuItem>
-                  <MenuItem value='Cooking'>Cooking</MenuItem>
-                  <MenuItem value='Travel'>Travel</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-
-            <Grid item xs={12} sm={6}>
-              <TextField
-                type="file"
-                name='image'
-                onChange={(e) => setImage(e.target.files[0])}
-              />
-            </Grid>
-
-            <Grid item xs={15}>
-              <Button variant='contained' sx={{ marginRight: 3.5 }} onClick={handleOnClick}>
-                Create Post
-              </Button>
-            </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label='Title'
+              name="title"
+              placeholder='Your title Is here'
+              onChange={handaleChange}
+            />
           </Grid>
-        </form>
-      </CardContent>
-    )
 
-    }
-    export default TabInfo
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              multiline
+              name="discription"
+              label='Description'
+              minRows={9}
+              onChange={handaleChange}
+              placeholder='Your content Is here'
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <InputLabel>Category</InputLabel>
+              <Select label='Category' name='category' >
+                <MenuItem value='Sports'>Sports</MenuItem>
+                <MenuItem value='Technology'>Technology</MenuItem>
+                <MenuItem value='Education'>Education</MenuItem>
+                <MenuItem value='Health'>Health</MenuItem>
+                <MenuItem value='Finance'>Finance</MenuItem>
+                <MenuItem value='Cooking'>Cooking</MenuItem>
+                <MenuItem value='Travel'>Travel</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <TextField
+              type="file"
+              name='image'
+              onChange={handleImageChange}
+            />
+          </Grid>
+
+          <Grid item xs={15}>
+            <Button variant='contained' sx={{ marginRight: 3.5 }} onClick={handleOnClick}>
+              Create Post
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </CardContent>
+  )
+
+}
+export default TabInfo
