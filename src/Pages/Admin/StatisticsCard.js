216d17008e32416528b917510d1145a3f8b7fb0f
Makarand Khiste <makarand.code@gmail.com>
2023-05-08 13:38:39 +0530

    Admin panel created

---
+++ src/Pages/Admin/StatisticsCard.js
@@ -0,0 +1,112 @@
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import Card from '@mui/material/Card'
+import Avatar from '@mui/material/Avatar'
+import CardHeader from '@mui/material/CardHeader'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import CardContent from '@mui/material/CardContent'
+import Table from 'src/Pages/Admin/Table'
+
+// ** Icons Imports
+import TrendingUp from 'mdi-material-ui/TrendingUp'
+import CurrencyUsd from 'mdi-material-ui/CurrencyUsd'
+import DotsVertical from 'mdi-material-ui/DotsVertical'
+import CellphoneLink from 'mdi-material-ui/CellphoneLink'
+import AccountOutline from 'mdi-material-ui/AccountOutline'
+import { Flag, Pen } from 'mdi-material-ui'
+import { BlockSharp, People } from '@material-ui/icons'
+
+const salesData = [
+  {
+    stats: '245',
+    title: 'Users',
+    color: 'primary',
+    icon: <People sx={{ fontSize: '1.75rem' }} />
+  },
+  {
+    stats: '1238',
+    title: 'Post',
+    color: 'success',
+    icon: <Pen sx={{ fontSize: '1.75rem' }} />
+  },
+  {
+    stats: '87',
+    color: 'warning',
+    title: 'Flag Posts',
+    icon: <Flag sx={{ fontSize: '1.75rem' }} />
+  },
+  {
+    stats: '23',
+    color: 'info',
+    title: 'Blocked Users',
+    icon: <BlockSharp sx={{ fontSize: '1.75rem' }} />
+  }
+]
+
+const renderStats = () => {
+  return salesData.map((item, index) => (
+    <Grid item xs={12} sm={3} key={index}>
+      <Box key={index} sx={{ display: 'flex', alignItems: 'center' }}>
+        <Avatar
+          variant='rounded'
+          sx={{
+            mr: 3,
+            width: 44,
+            height: 44,
+            boxShadow: 3,
+            color: 'common.white',
+            backgroundColor: `${item.color}.main`
+          }}
+        >
+          {item.icon}
+        </Avatar>
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <Typography variant='caption'>{item.title}</Typography>
+          <Typography variant='h6'>{item.stats}</Typography>
+        </Box>
+      </Box>
+    </Grid>
+  ))
+}
+
+const StatisticsCard = () => {
+  return (
+    <>
+      <Card sx={{marginBottom:'20px'}}>
+        <CardHeader
+          title='Statistics Card'
+          action={
+            <IconButton size='small' aria-label='settings' className='card-more-options' sx={{ color: 'text.secondary' }}>
+              <DotsVertical />
+            </IconButton>
+          }
+          subheader={
+            <Typography variant='body2'> 
+              <Box component='span' sx={{ fontWeight: 600, color: 'text.primary' }}>
+              Total Users, Posts, and the Flag Posts 
+              </Box>{' '}
+              of the day
+            </Typography>
+          }
+          titleTypographyProps={{
+            sx: {
+              mb: 2.5,
+              lineHeight: '2rem !important',
+              letterSpacing: '0.15px !important'
+            }
+          }}
+        />
+        <CardContent sx={{ pt: theme => `${theme.spacing(3)} !important` }}>
+          <Grid container spacing={[5, 0]}>
+            {renderStats()}
+          </Grid>
+        </CardContent>
+      </Card>
+      <Table/>
+    </>    
+  )
+}
+
+export default StatisticsCard
---
+++ src/Pages/Admin/Table.js
@@ -0,0 +1,141 @@
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Card from '@mui/material/Card'
+import Chip from '@mui/material/Chip'
+import Table from '@mui/material/Table'
+import TableRow from '@mui/material/TableRow'
+import TableHead from '@mui/material/TableHead'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import Typography from '@mui/material/Typography'
+import TableContainer from '@mui/material/TableContainer'
+import { CheckBold, DeleteAlert, DeleteForever, EyeCircle } from 'mdi-material-ui'
+import { Block } from '@material-ui/icons'
+
+const rows = [
+  {
+    age: 27,
+    status: 'current',
+    date: '09/27/2018',
+    name: 'Sally Quinn',
+    salary: '$19586.23',
+    email: 'eebsworth2m@sbwire.com',
+    designation: 'Human Resources Assistant'
+  },
+  {
+    age: 61,
+    date: '09/23/2016',
+    salary: '$23896.35',
+    status: 'professional',
+    name: 'Margaret Bowers',
+    email: 'kocrevy0@thetimes.co.uk',
+    designation: 'Nuclear Power Engineer'
+  },
+  {
+    age: 59,
+    date: '10/15/2017',
+    name: 'Minnie Roy',
+    status: 'rejected',
+    salary: '$18991.67',
+    email: 'ediehn6@163.com',
+    designation: 'Environmental Specialist'
+  },
+  {
+    age: 30,
+    date: '06/12/2018',
+    status: 'resigned',
+    salary: '$19252.12',
+    name: 'Ralph Leonard',
+    email: 'dfalloona@ifeng.com',
+    designation: 'Sales Representative'
+  },
+  {
+    age: 66,
+    status: 'applied',
+    date: '03/24/2018',
+    salary: '$13076.28',
+    name: 'Annie Martin',
+    designation: 'Operator',
+    email: 'sganderton2@tuttocitta.it'
+  },
+  {
+    age: 33,
+    date: '08/25/2017',
+    salary: '$10909.52',
+    name: 'Adeline Day',
+    status: 'professional',
+    email: 'hnisius4@gnu.org',
+    designation: 'Senior Cost Accountant'
+  },
+  {
+    age: 61,
+    status: 'current',
+    date: '06/01/2017',
+    salary: '$17803.80',
+    name: 'Lora Jackson',
+    designation: 'Geologist',
+    email: 'ghoneywood5@narod.ru'
+  },
+  {
+    age: 22,
+    date: '12/03/2017',
+    salary: '$12336.17',
+    name: 'Rodney Sharp',
+    status: 'professional',
+    designation: 'Cost Accountant',
+    email: 'dcrossman3@google.co.jp'
+  }
+]
+
+const statusObj = {
+  applied: { color: 'info' },
+  rejected: { color: 'error' },
+  current: { color: 'primary' },
+  resigned: { color: 'warning' },
+  professional: { color: 'success' }
+}
+
+const DashboardTable = () => {
+  return (
+    <Card>
+      <TableContainer>
+        <Table sx={{ minWidth: 800 }} aria-label='table in dashboard'>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Block</TableCell>
+              <TableCell>Unblock</TableCell>
+              <TableCell>View All Posts</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow hover key={row.name} sx={{ '&:last-of-type td, &:last-of-type th': { border: 0 } }}>
+                <TableCell sx={{ py: theme => `${theme.spacing(0.5)} !important` }}>
+                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <Typography sx={{ fontWeight: 500, fontSize: '0.875rem !important' }}>{row.name}</Typography>
+                    <Typography variant='caption'>{row.designation}</Typography>
+                  </Box>
+                </TableCell>
+                <TableCell>{row.email}</TableCell>
+                <TableCell>
+                <Button variant="outlined" color='error' startIcon={<Block />}>Block</Button>
+                </TableCell>
+                <TableCell>
+                <Button variant="outlined" color='success' startIcon={<CheckBold />}>Unblock</Button>
+                </TableCell>
+                <TableCell>
+                <Button variant="outlined" color='info' startIcon={<EyeCircle />}>View All</Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Card>
+  )
+}
+
+export default DashboardTable
