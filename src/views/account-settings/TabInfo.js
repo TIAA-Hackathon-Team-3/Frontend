54d7caf7d628d7dc459e97ea92ed453d95a9bcfc
Akash-Tandale001 <ak.tandale123@gmail.com>
2023-05-08 16:02:46 +0530

    update

--- src/views/account-settings/TabInfo.js
+++ src/views/account-settings/TabInfo.js
@@ -23,7 +23,7 @@ const TabInfo = () => {
     title: "",
     discription: "",
     image: "",
-    category: "health"
+    category: "Education"
   })
   const { loginAuth } = useSelector(state => state.auth);
   const router = useRouter()
